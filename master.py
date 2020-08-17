06ec1c33b2b3caaef3e4703e563305b999004861
anishvm <45866770+anishvm@users.noreply.github.com>
2020-08-17 19:49:08 +0530

    Update master.py

--- master.py
+++ master.py
@@ -5,7 +5,7 @@ print("The time of code execution begin is : ", end ="")
 print(time.ctime()) 
   
 # using sleep() to hault the code execution 
-time.sleep(60) 
+time.sleep(3600) 
   
 # printing the end time  
 print("The time of code execution end is : ", end ="") 
