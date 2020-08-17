33c1c61569bddb2c89cf82fda88cd129ebf7f1c8
anishvm <45866770+anishvm@users.noreply.github.com>
2020-08-17 19:15:56 +0530

    Add files via upload

---
+++ master.py
@@ -0,0 +1,12 @@
+import time 
+  
+# printing the start time  
+print("The time of code execution begin is : ", end ="") 
+print(time.ctime()) 
+  
+# using sleep() to hault the code execution 
+time.sleep(60) 
+  
+# printing the end time  
+print("The time of code execution end is : ", end ="") 
+print(time.ctime())
